d86cd24715b8fa578ed5fdc5090c3dad3e346fb0
NasrinShakery <nasrin.shakery8@gmail.com>
2024-01-29 18:57:22 +0330

    tailwind installed

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <>
+      <div>Navbar</div>
+      <h1 className="text-3xl text-blue-500 bg-orange-200 font-bold underline">
+         Hello world!
+      </h1>
+   </>
+  )
+}
+
+export default Navbar
