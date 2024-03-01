75c40d26f3a525cae160d6b6bdd010071fe03dbb
NasrinShakery <nasrin.shakery8@gmail.com>
2024-03-01 23:27:26 +0330

    :style: userList

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <>
+    <h3 className='p-4'>لیست کاربران سایت</h3>
+      <div className='grid grid-cols-9'>
+        <div className='col-span-1 border p-2 bg-blueSpace'> شماره </div>
+        <div className='col-span-2 border p-2 bg-blueSpace'>نام کاربر</div>
+        <div className='col-span-2 border p-2 bg-blueSpace'> ایمیل </div>
+        <div className='col-span-2 border p-2 bg-blueSpace'>موبایل</div>
+        <div className='col-span-1 border p-2 bg-blueSpace'>ویرایش</div>
+        <div className='col-span-1 border p-2 bg-blueSpace'>حذف</div>
+      </div>
+      <div className='grid grid-cols-9'>
+        <div className='col-span-1 border p-2'> شماره </div>
+        <div className='col-span-2 border p-2'>نام کاربر</div>
+        <div className='col-span-2 border p-2'> ایمیل </div>
+        <div className='col-span-2 border p-2'>موبایل</div>
+        <div className='col-span-1 border p-2'>ویرایش</div>
+        <div className='col-span-1 border p-2'>حذف</div>
+      </div>
+     
+    </>
   )
 }
 
